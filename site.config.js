4f5ecb453610dc8f0db18c631778d6d75fa68b14
3gbCyber <57197804+3gbCyber@users.noreply.github.com>
2023-04-29 00:01:31 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,12 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "AboYaqoub",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "Well idk what I can write.",
+    email: "xdfir@proton.me",
+    linkedin: "",
+    github: "3gbCyber",
     instagram: "",
   },
   projects: [
@@ -18,13 +17,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "AboYaqoub",
+    description: "welcome to AboYaqoub Blog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://3gbcyber.vercel.app/",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
